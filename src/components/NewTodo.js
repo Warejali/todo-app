bc578f97ae9cc3f4878d950983b733f13a3118a4
Md Warej Ali <waraj.com@gmail.com>
2022-05-19 06:51:07 +0600

    final done

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -15,16 +15,18 @@ const NewTodo = ({ handleTodo }) => {
 
     const formSubmit = event => {
         event.preventDefault();
-        handleTodo(todo)
+        handleTodo(todo);
+        setTodo({ title: "", desc: "" });
         toast.success('Successfully Added')
+
     }
 
 
     return (
-        <div className='card bg-slate-600 shadow-xl py-5'>
+        <div className='card bg-slate-600 shadow-xl py-5 lg:w-96 mx-auto'>
             <form onSubmit={formSubmit} className='flex flex-col gap-5 mx-auto justify-center items-center lg:w-96'>
-                <input onChange={handleChange} type="text" name='title' placeholder="Type title of todo" class="input input-bordered input-primary w-full max-w-xs" />
-                <input onChange={handleChange} type="text" name='desc' placeholder="Type description of todo" class="input input-bordered input-primary w-full max-w-xs" />
+                <input onChange={handleChange} type="text" name='title' placeholder="Type title of todo" class="input input-bordered input-primary w-full max-w-xs" required />
+                <input onChange={handleChange} type="text" name='desc' placeholder="Type description of todo" class="input input-bordered input-primary w-full max-w-xs" required />
                 <button className='btn btn-primary btn-sm'>Add NEW TO DO</button>
             </form>
         </div>
